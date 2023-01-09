42f3e10d62ce514df93f9cb1cc6a2c8c44d23167
Charlie Mitts <charlie11138@gmail.com>
2023-01-09 13:18:30 -0700

    Assignment finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+        int finished = 0;
+        while (finished == 0)
+        {
+            Console.Write("What is your guess? ");
+            string answer2 = Console.ReadLine();
+             int guess = int.Parse(answer2);
+
+            if (guess > number)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (guess < number)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                finished = 1;
+            }
+        }
     }
 }
