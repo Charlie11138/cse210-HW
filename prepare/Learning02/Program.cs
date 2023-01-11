10935b7e7698be5e57bdc279f005b3017ba988d7
Charlie Mitts <charlie11138@gmail.com>
2023-01-11 13:46:58 -0700

    Started

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Team Member";
+        job1._company = "Chick-fil-A";
+        job1._startYear = 2011;
+        job1._endYear = 2014;
     }
 }
