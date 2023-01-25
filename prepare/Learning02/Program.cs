131aff69ef29909411b78000cea26c7c8c7b2a70
Charlie Mitts <charlie11138@gmail.com>
2023-01-25 12:49:06 -0700

    Added 3 Job

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -15,11 +15,18 @@ class Program
         job2._company = "Chick-fil-A";
         job2._startYear = 2014;
         job2._endYear = 2017;
+
+        Job job3 = new Job();
+        job3._jobTitle = "Picker";
+        job3._company = "TVH";
+        job3._startYear = 2015;
+        job3._endYear = 2016;
         
         Resume resume1 = new Resume();
         resume1._name = "Charlie Mitts";
         resume1._jobs.Add(job1);
         resume1._jobs.Add(job2);
+        resume1._jobs.Add(job3);
         resume1.Display();
     }
     
